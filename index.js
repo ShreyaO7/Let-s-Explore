134216949d9fe696f8b49e807b1f924bae493da2
Shreya ojha <shreyaojha192027@gmail.com>
2024-10-17 11:01:33 +0530

    all files

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")))
 
 app.use(express.urlencoded({extended:true})); //data getting parsed
 
